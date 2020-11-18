81d5f251e916c8ae76e89ebbbde0d7ad69394b8a
Yair <32864499+bluepostit@users.noreply.github.com>
2020-11-18 11:00:05 +0200

    member, collection routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'top'
+    end
+
+    member do
+      get 'chef'
+    end
+  end
+  # get 'restaurants/top', to: 'restaurants#top', as: :top_restaurants
+  # get 'restaurants/:id/chef', to: 'restaurants#chef', as: :chef_restaurant
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts 'Adding restaurants...'
+taco_bell = {
+  name: 'Taco Bell',
+  address: '23 Main Way',
+  stars: 2
+}
+
+mcd = {
+  name: 'McDonald\'s',
+  address: '1 Central Avenue',
+  stars: 3
+}
+
+Restaurant.create!(taco_bell)
+Restaurant.create!(mcd)
+
+
+puts 'Done.'
